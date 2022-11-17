d45ed203eb862aab48cffca9422e0dd5e5b18563
Wanhee Lee <wanhee@stanford.edu>
2022-11-16 17:06:22 -0800

    Before debugging feature encoding on 1116

--- run_msn_raybatch4096.sh
+++ run_msn_raybatch4096.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-python train/train.py -n msn_train_raybatch4096 -c conf/exp/msn.conf \
-    -D /ccn2/u/honglinc/datasets/multishapenet -V 1 --gpu_id='0 1 2 3' \
-    --ray_batch_size 4096 --batch_size 8
+python train/train.py -n msn_train_raybatch4096_1115 -c conf/exp/msn.conf \
+    -D /ccn2/u/honglinc/datasets/multishapenet -V 1 --gpu_id='0 1 2 3 4 5 6 7' \
+    --ray_batch_size 4096 --batch_size 16
     # --fg_mask
     # --n_scenes 6229 --skip 400
